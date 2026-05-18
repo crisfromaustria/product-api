d260af47e898fddd4f8b4701c5eb08a55e279a3b
cris <christoph@takacs.at>
2026-05-18 16:10:52 +0200

    kubernetes deployment und service, id/port ausgabe auf root

--- src/main/java/at/fontain/liveness/LivenessRest.java
+++ src/main/java/at/fontain/liveness/LivenessRest.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.health.HealthContributorRegistry;
 import org.springframework.boot.actuate.health.HealthEndpointGroups;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/simulate")
 public class LivenessRest {
     private static final Logger log = LoggerFactory.getLogger(LivenessRest.class);
 
@@ -16,20 +19,23 @@ public class LivenessRest {
     private final CustomHealthIndicator health;
     private final HealthEndpointGroups groups;
     private final HealthContributorRegistry registry;
+    private final ServerProperties properties;
 
     public LivenessRest(ToggleableReadinessIndicator readiness,
                         ToggleableLivenessIndicator liveness,
                         CustomHealthIndicator health,
                         HealthEndpointGroups groups,
-                        HealthContributorRegistry registry) {
+                        HealthContributorRegistry registry,
+                        ServerProperties properties) {
         this.readiness = readiness;
         this.liveness = liveness;
         this.health = health;
         this.groups = groups;
         this.registry = registry;
+        this.properties = properties;
     }
 
-    @GetMapping("/readiness/{state}")
+    @GetMapping("/simulate/readiness/{state}")
     public String setReadiness(@PathVariable boolean state) {
         log.info("setReadiness({})", state);
 
@@ -37,7 +43,7 @@ public class LivenessRest {
         return "Readiness set to " + state;
     }
 
-    @GetMapping("/liveness/{state}")
+    @GetMapping("/simulate/liveness/{state}")
     public String setLiveness(@PathVariable boolean state) {
         log.info("setLiveness({})", state);
 
@@ -45,11 +51,23 @@ public class LivenessRest {
         return "Liveness set to " + state;
     }
 
-    @GetMapping("/health/{state}")
+    @GetMapping("/simulate/health/{state}")
     public String setHealth(@PathVariable boolean state) {
         log.info("setHealth({})", state);
 
         health.setHealthy(state);
         return "Health set to " + state;
+    }
+
+    @GetMapping("/")
+    public List<String> root() {
+        String hostname = properties.getAddress().getHostName();
+        String hostaddress = properties.getAddress().getHostAddress();
+        int port = properties.getPort();
+        List<String> list = new ArrayList<>();
+        list.add("hostname: " + hostname);
+        list.add("hostaddress: " + hostaddress);
+        list.add("port: " + port);
+        return list;
     }
 }
